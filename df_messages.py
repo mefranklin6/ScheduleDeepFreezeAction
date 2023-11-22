131c97a21c7ae5001e0f57f73ac34e95fad3a3ed
mefranklin6 <mefranklin@csuchico.edu>
2023-11-21 16:58:57 -0800

    debugged messages

--- df_messages.py
+++ df_messages.py
@@ -8,29 +8,40 @@ Please contact {support_email} for any changes or issues'''
 
 ###############################################################################
 
-def make_force_description(force:bool):
+
+def make_force_description(force: bool):
     if force == False:
         force_description = '''Please make sure all programs on the PC are closed, including Zoom, PowerPoint, etc.
 To prevent disruption, automatic action will not be taken if there are signs of user activity.
 To skip checking, please re-schedule and set the 'Force' flag.'''
-    
+
     if force == True:
         force_description = '''Warning: Force flag is set.
 Reboot commands will be sent REGARDLESS of user activity on the machine'''
-    
+
     return force_description
 
 ###############################################################################
 
-def make_schedule_email(force_description, footer):
-    scheduled_email = f"""From: {config['Emails']['From_Name']}
-To: {inputValues.requestor_email}
+
+def make_schedule_email(
+        force_description,
+        footer,
+        from_name,
+        to_email,
+        pc,
+        desired_state,
+        scheduled_time
+):
+
+    scheduled_email = f"""From: {from_name}
+To: {to_email}
 Subject: Deep Freeze Action Scheduled
 
 
-PC: {inputValues.pc_name}
-Scheduled to reboot {inputValues.status}
-At: {inputValues.time}
+PC: {pc}
+Scheduled to reboot {desired_state}
+At: {scheduled_time}
 
 You will receive a follow up email once action has been taken.
 
@@ -42,14 +53,15 @@ You will receive a follow up email once action has been taken.
 
 ###############################################################################
 
-def make_success_email(footer):
-    success_email = f"""From: {config['Emails']['From_Name']}
-To: {inputValues.requestor_email}
+
+def make_success_email(from_name, to_email, pc, confirmed_state, footer):
+    success_email = f"""From: {from_name}
+To: {to_email}
 Subject: Deep Freeze Action Sucessful
 
 
-PC: {inputValues.pc_name}
-Has Sucessfully Rebooted {inputValues.status}
+PC: {pc}
+Has Sucessfully Rebooted {confirmed_state}
 
 
 {footer}
@@ -58,14 +70,22 @@ Has Sucessfully Rebooted {inputValues.status}
 
 ###############################################################################
 
-def make_failure_email(footer, log_data):
-    failure_email = f"""From: {config['Emails']['From_Name']}
-To: {inputValues.requestor_email}
+
+def make_failure_email(
+        from_name,
+        to_email,
+        pc,
+        desired_state,
+        footer,
+        log_data
+):
+    failure_email = f"""From: {from_name}
+To: {to_email}
 Subject: Deep Freeze Action FAILURE
 
 
-PC: {inputValues.pc_name}
-FAILED to reboot {inputValues.status}
+PC: {pc}
+FAILED to reboot {desired_state}
 
 Manual action may be required.
 
@@ -80,4 +100,3 @@ Log:
     return failure_email
 
 ###############################################################################
-
